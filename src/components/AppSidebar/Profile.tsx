fe0e24cad8bdcf97f89c4f3c3572a4e054ab38bb
Ukanah Dean <ukanah15thdean@gmail.com>
2025-05-23 11:15:39 +0100

    feat: implemented update profile image functionality

--- src/components/AppSidebar/Profile.tsx
+++ src/components/AppSidebar/Profile.tsx
@@ -1,18 +1,40 @@
+import { useGetUserProfileImageQuery } from "@/hooks/user"
+import { useMemo } from "react"
+import { Skeleton } from "../ui/skeleton"
+import { User } from "lucide-react"
+
 interface ProfileProps {
-  avatar: string
   name: string
   email: string
 }
 
-export function Profile({ avatar, name, email }: ProfileProps) {
+export function Profile({ name, email }: ProfileProps) {
+  const { data: profileImageData, isPending: isLoadingProfileImage } =
+    useGetUserProfileImageQuery()
+
+  const profileImage = useMemo(
+    () => profileImageData?.imageUrl,
+    [profileImageData]
+  )
+
+  if (isLoadingProfileImage) {
+    return <Skeleton className="w-10 h-10 rounded-full" />
+  }
+
   return (
     <div className="flex items-center gap-3 p-2 rounded-lg hover:bg-gray-100 transition-colors pointer-events-none select-none">
       <div className="relative">
-        <img
-          src={avatar}
-          alt={name}
-          className="w-10 h-10 rounded-full object-cover border-2 border-white shadow-sm"
-        />
+        {profileImage ? (
+          <img
+            src={profileImage}
+            alt={name}
+            className="w-10 h-10 rounded-full object-cover border-2 border-white shadow-sm"
+          />
+        ) : (
+          <div className="flex items-center justify-center w-9 h-9 rounded-full bg-muted">
+            <User className="size-6 text-muted-foreground" />
+          </div>
+        )}
         <div className="absolute bottom-0 right-0 w-3 h-3 bg-green-500 rounded-full border-2 border-white"></div>
       </div>
 
--- src/views/mail/settings/views/Subname/components/FormSection.tsx
+++ src/views/mail/settings/views/Subname/components/FormSection.tsx
@@ -1,26 +1,95 @@
+import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useToastContext } from "@/components/ui/toast"
+import { useSetUserProfileImageMutation } from "@/hooks/user"
+import { uploadToCloudinary } from "@/utils/cloudinary"
+import { useQueryClient } from "@tanstack/react-query"
 import { Upload, User } from "lucide-react"
-import { useState } from "react"
+import { useMemo, useState } from "react"
 
-export function FormSection() {
-  const [profileImage, setProfileImage] = useState<string | null>(null)
+export function FormSection({
+  activeUserProfileImage,
+}: {
+  activeUserProfileImage: string | null
+}) {
+  const [isUploading, setIsUploading] = useState(false)
 
-  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const queryClient = useQueryClient()
+  const [imageRef, setImageRef] = useState<string | null>(null)
+  const [profileImage, setProfileImage] = useState<File | null>(null)
+
+  const { mutateAsync: setUserProfileImage } = useSetUserProfileImageMutation()
+
+  const { setNotification } = useToastContext()
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0]
+
     if (file) {
-      const imageUrl = URL.createObjectURL(file)
-      setProfileImage(imageUrl)
+      if (file.size > 1024 * 1024) {
+        setNotification({
+          message: "File too large",
+          description: "Please select an image smaller than 1MB",
+          type: "error",
+        })
+        return
+      }
+
+      setImageRef(URL.createObjectURL(file))
+      setProfileImage(file)
     }
   }
+
+  const handleImageUpload = async () => {
+    if (!profileImage) {
+      setNotification({
+        message: "No image selected",
+        description: "Please select an image to upload",
+        type: "error",
+      })
+      return
+    }
+
+    try {
+      setIsUploading(true)
+      const url = await uploadToCloudinary(profileImage)
+
+      await setUserProfileImage(url).then(async () => {
+        setNotification({
+          message: "Image uploaded successfully",
+          description: "Your profile image has been updated",
+          type: "success",
+        })
+        await queryClient.invalidateQueries({
+          queryKey: ["user-profile-image"],
+        })
+        setImageRef(null)
+        setProfileImage(null)
+      })
+    } catch {
+      setNotification({
+        message: "Failed to upload image",
+        description: "Please try again",
+        type: "error",
+      })
+    } finally {
+      setIsUploading(false)
+    }
+  }
+
+  const activeUserImage = useMemo(
+    () => imageRef || activeUserProfileImage,
+    [activeUserProfileImage, imageRef]
+  )
 
   return (
     <form className="flex flex-col space-y-6">
       <div className="w-fit flex flex-col items-center space-y-4">
         <div className="relative h-32 w-32 rounded-full overflow-hidden border-2 border-muted bg-muted flex items-center justify-center">
-          {profileImage ? (
+          {activeUserImage ? (
             <img
-              src={profileImage || "/placeholder.svg"}
+              src={activeUserImage}
               alt="Profile preview"
               className="object-cover"
             />
@@ -28,7 +97,7 @@ export function FormSection() {
             <User className="h-16 w-16 text-muted-foreground" />
           )}
         </div>
-        <div className="flex items-center">
+        <div className="flex flex-col items-center">
           <Label
             htmlFor="profile-image"
             className="cursor-pointer inline-flex items-center gap-2 px-4 py-2 rounded-md bg-secondary text-secondary-foreground hover:bg-secondary/80"
@@ -41,8 +110,19 @@ export function FormSection() {
             type="file"
             accept="image/*"
             className="hidden"
-            onChange={handleImageUpload}
+            onChange={handleFileChange}
           />
+
+          {profileImage && (
+            <Button
+              type="button"
+              onClick={handleImageUpload}
+              disabled={isUploading}
+              className="w-full bg-emerald-500 hover:bg-emerald-600 mt-3"
+            >
+              {isUploading ? "Uploading..." : "Upload"}
+            </Button>
+          )}
         </div>
       </div>
 
